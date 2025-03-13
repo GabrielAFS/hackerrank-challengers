cf868c8c1450491498d1697e51a9a7ef916a3a74
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-13 16:34:19 -0300

    chore: Add linked lists challenges resolutions

---
+++ linked-lists/insert-to-tail/index.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+sys.setrecursionlimit(999999)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head: SinglyLinkedListNode, data: int):
+    if head:
+        head.next = insertNodeAtTail(head.next, data)
+    else:
+        head = SinglyLinkedListNode(data)
+        
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
